014e940c2b0d7e8fc335aea90dba79a971da8d8e
Egor Petushenko <chumicheva-yulia19@yandex.ru>
2023-01-22 12:59:16 +0300

    added nojekyll

--- script/script.js
+++ script/script.js
@@ -1,34 +1,29 @@
-console.log('hello');
 let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
 let popupBtnClose = document.querySelector(".popup__close");
-let popupBtnSave = document.querySelector(".popup__submit-button");
 let formElement  = document.querySelector(".popup__form");
-let nameInput  = document.querySelector(".popup__user-name");
-let jobInput  = document.querySelector(".popup__user-info");
+let nameInput  = document.querySelector(".popup__form-input_user-name");
+let jobInput  = document.querySelector(".popup__form-input_user-info");
 let profileName = document.querySelector(".profile__user-name");
 let profileInfo = document.querySelector(".profile__user-info");
 
-editButton.addEventListener("click", openPopup);
-
 function openPopup(){
   popup.classList.add("popup_opened");
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = jobInput.value;
 }
-
-popupBtnClose.addEventListener("click", closePopup);
 
 function closePopup(){
   popup.classList.remove("popup_opened");
 }
 
-popupBtnSave.addEventListener("click", closePopup);
-
-
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
 }
 
+editButton.addEventListener("click", openPopup);
+popupBtnClose.addEventListener("click", closePopup);
 formElement.addEventListener('submit', handleFormSubmit); 
