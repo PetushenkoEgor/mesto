ddccac483ff6e2e64aa311087c9561e8eee8c6d5
Egor Petushenko <chumicheva-yulia19@yandex.ru>
2023-01-24 22:11:51 +0300

    prod.4.4

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let popupBtnClose = document.querySelector(".popup__close");
 let formElement  = document.querySelector(".popup__form");
-let nameInput  = document.querySelector(".popup__form-input_user-name");
-let jobInput  = document.querySelector(".popup__form-input_user-info");
+let nameInput  = document.querySelector(".popup__form-input_user_name");
+let jobInput  = document.querySelector(".popup__form-input_user_info");
 let profileName = document.querySelector(".profile__user-name");
 let profileInfo = document.querySelector(".profile__user-info");
 
@@ -21,7 +21,7 @@ function handleFormSubmit (evt) {
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
-  popup.classList.remove("popup_opened");
+  closePopup();
 }
 
 editButton.addEventListener("click", openPopup);
